38860b2f6bed5ffb0747802b4e66eec20166d39a
JohnONolan <JohnONolan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-23 19:35:52 +0000

    Add support for all types of post statuses. Fixes #1290
    git-svn-id: https://plugins.svn.wordpress.org/ui-labs/trunk@363908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ui-labs.php
+++ ui-labs.php
@@ -4,13 +4,32 @@ Plugin Name: UI Labs
 Plugin URI: 
 Description: Experimental WordPress admin UI features, ooo shiny!
 Author: John O'Nolan
-Version: 1.1
+Version: 1.0.1
 Author URI: http://john.onolan.org
 */
 
 /**
+ * Modify the output of post statuses
+ *
+ * Allows for fine-grained control of styles and targetin protected
+ * posts, written by Pete Mall
+ *
+ * @since 1.0.1
+ */
+function labs_display_post_states( $post_states ) {
+   foreach ( $post_states as &$post_state )
+       $post_state = '<span class="' . strtolower( str_replace( ' ', '-', $post_state ) ) . '">' . $post_state . '</span>';
+   return $post_states;
+}
+
+add_filter( 'display_post_states', 'labs_display_post_states' );
+
+/**
  * Add CSS file link
+ *
  * Based on the WP Admin Theme plugin by David Smith
+ *
+ * @since 1.0
  */
 function ui_labs_css() {
 	$url = plugins_url('/ui-labs.css', __FILE__);
