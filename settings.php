78c51c5f2470c10d63fdf7d5eea3bf800b3af20f
JohnONolan <JohnONolan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-04 01:49:14 +0000

    Removing dirty shorthand php. Ollie.
    git-svn-id: https://plugins.svn.wordpress.org/ui-labs/trunk@446963 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -6,26 +6,26 @@
     <table class="form-table">
         <tr valign="top">
         <th scope="row"><strong>Experiment 1:</strong> Colour-coded Post Statuses</th>
-        <td><input type="checkbox" name="poststatuses" value="yes"<?=get_option('poststatuses') == 'yes' ? ' checked' : '';?> /></td>
+        <td><input type="checkbox" name="poststatuses" value="yes"<?php echo get_option('poststatuses') == 'yes' ? ' checked' : '';?> /></td>
         </tr>
          
         <tr valign="top">
         <th scope="row"><strong>Experiment 2:</strong> Classic WordPress Admin Bar</th>
-        <td><input type="checkbox" name="adminbar" value="yes"<?=get_option('adminbar') == 'yes' ? ' checked' : '';?> /></td>
+        <td><input type="checkbox" name="adminbar" value="yes"<?php echo get_option('adminbar') == 'yes' ? ' checked' : '';?> /></td>
         </tr>
         
         <tr valign="top">
         <th scope="row"><strong>Experiment 3:</strong> Identify This Server</th>
-        <td><input type="checkbox" name="identify" value="yes"<?=get_option('identify') == 'yes' ? ' checked' : '';?> /></td>
+        <td><input type="checkbox" name="identify" value="yes"<?php echo get_option('identify') == 'yes' ? ' checked' : '';?> /></td>
         </tr>
         <tr valign="top">
         <th scope="row">Server type</th>
         <td>
         	<select id="servertype" name="servertype">
-        		<option value="uilabs-blank"<?=get_option('servertype') == 'uilabs-blank' ? ' selected' : '';?>>--</option>
-        		<option value="uilabs-development"<?=get_option('servertype') == 'uilabs-development' ? ' selected' : '';?>>Development</option>
-        		<option value="uilabs-staging"<?=get_option('servertype') == 'uilabs-staging' ? ' selected' : '';?>>Staging</option>
-        		<option value="uilabs-live"<?=get_option('servertype') == 'uilabs-live' ? ' selected' : '';?>>Live</option>
+        		<option value="uilabs-blank"<?php echo get_option('servertype') == 'uilabs-blank' ? ' selected' : '';?>>--</option>
+        		<option value="uilabs-development"<?php echo get_option('servertype') == 'uilabs-development' ? ' selected' : '';?>>Development</option>
+        		<option value="uilabs-staging"<?php echo get_option('servertype') == 'uilabs-staging' ? ' selected' : '';?>>Staging</option>
+        		<option value="uilabs-live"<?php echo get_option('servertype') == 'uilabs-live' ? ' selected' : '';?>>Live</option>
         	</select>
         </td>
         </tr>
